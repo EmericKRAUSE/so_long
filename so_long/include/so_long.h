f0c1f4df924089bebd1b049f20c6ecc7aa287684
Emeric Krause <ekrause@k0r4p5.42mulhouse.fr>
2024-01-23 11:25:19 +0100

    movement, collision, collect and win condition is finished, just need to do some adjustement

--- so_long/include/so_long.h
+++ so_long/include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/17 13:01:05 by ekrause           #+#    #+#             */
-/*   Updated: 2024/01/22 11:35:03 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/01/23 10:47:07 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,7 @@ typedef struct player
 {
 	int	y;
 	int x;
+	int collectible;
 }		t_player;
 
 /****
--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: ekrause <emeric.yukii@gmail.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 14:26:25 by ekrause           #+#    #+#             */
-/*   Updated: 2024/01/22 15:17:28 by ekrause          ###   ########.fr       */
+/*   Updated: 2024/01/23 11:22:56 by ekrause          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 t_image_package	g_images;
 t_map			g_map;
+t_player		g_player;
 int				g_pixels = 124;
 
 t_image_package	init_image_package(mlx_t *mlx)
@@ -35,23 +36,26 @@ t_image_package	init_image_package(mlx_t *mlx)
 	return (images);
 }
 
-void	display_character(mlx_t *mlx)
+t_player	init_player()
 {
-	int	y;
-	int	x;
+	t_player	player;
 
-	y = get_position(g_map, 'y');
-	x = get_position(g_map, 'x');
+	player.y = get_position(g_map, 'y');
+	player.x = get_position(g_map, 'x');
+	player.collectible = 0;
+	return (player);
+}
+
+void	display_character(mlx_t *mlx, int y, int x)
+{
 	mlx_image_to_window(mlx, g_images.image_character, x * g_pixels, y * g_pixels);
 }
 
-void	display_map(void *param)
+void	display_map(mlx_t *mlx)
 {
-	mlx_t	*mlx;
 	int		y;
 	int		x;
 
-	mlx = param;
 	g_images = init_image_package(mlx);
 
 	mlx_resize_image(g_images.image_background, g_pixels, g_pixels);
@@ -73,13 +77,10 @@ void	display_map(void *param)
 				mlx_image_to_window(mlx, g_images.image_collectible, x * g_pixels, y * g_pixels);
 			else if (g_map.tab[y][x] == 'E')
 				mlx_image_to_window(mlx, g_images.image_exit, x * g_pixels, y * g_pixels);
-			else if (g_map.tab[y][x] == 'P')
-				mlx_image_to_window(mlx, g_images.image_character, x * g_pixels, y * g_pixels);
 			x++;
 		}
 		y++;
 	}
-	//display_character(mlx);
 }
 
 void	delete_map(mlx_t *mlx)
@@ -91,40 +92,60 @@ void	delete_map(mlx_t *mlx)
 	mlx_delete_image(mlx, g_images.image_character);
 }
 
-void	key_event(mlx_t *mlx, int y, int x)
+void	is_collectible(mlx_t *mlx, int y, int x)
 {
-		g_map.tab[y][x] = 'P';
-		delete_map(mlx);
-		display_map(mlx);
+		if (g_map.tab[y][x] == 'C')
+		{
+			g_map.tab[y][x] = '0';
+			delete_map(mlx);
+			display_map(mlx);
+			display_character(mlx, y, x);
+			g_player.collectible++;
+		}
+}
+
+void	is_exit(mlx_t *mlx, int y, int x)
+{
+	if (g_map.tab[y][x] == 'E' && g_player.collectible == g_map.collectible)
+	{
+		mlx_texture_t *win_texture = mlx_load_png("../assets/win2.png");
+		mlx_image_t *win_image = mlx_texture_to_image(mlx, win_texture);
+		mlx_resize_image(win_image, 1920 / 2, 1080 / 2);
+		mlx_image_to_window(mlx, win_image, 1920 / 2, 1080 / 2);
+	}
 }
 
 void	key_hook(mlx_key_data_t keydata, void *param)
 {
 	mlx_t *mlx;
 
-	int y = g_images.image_character->instances->y / g_pixels;
+	int	y = g_images.image_character->instances->y / g_pixels;
 	int x = g_images.image_character->instances->x / g_pixels;
 
 	mlx = param;
 	if (keydata.key == MLX_KEY_W && keydata.action == MLX_PRESS && g_map.tab[y - 1][x] != '1')
 	{
-		g_map.tab[y][x] = '0';
-		key_event(mlx, y - 1, x);
+		g_images.image_character->instances->y -= g_pixels;
+		is_collectible(mlx, y - 1, x);
+		is_exit(mlx, y - 1, x);
 	}
-	if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS && g_map.tab[y + 1][x] != '1')
+	else if (keydata.key == MLX_KEY_S && keydata.action == MLX_PRESS && g_map.tab[y + 1][x] != '1')
 	{
-		g_map.tab[y][x] = '0';
-		key_event(mlx, y + 1, x);
+		g_images.image_character->instances->y += g_pixels;
+		is_collectible(mlx, y + 1, x);
+		is_exit(mlx, y + 1, x);
 	}
-	if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS && g_map.tab[y][x - 1] != '1')
+	else if (keydata.key == MLX_KEY_A && keydata.action == MLX_PRESS && g_map.tab[y][x - 1] != '1')
 	{
-		g_map.tab[y][x] = '0';
-		key_event(mlx, y, x - 1);
+		g_images.image_character->instances->x -= g_pixels;
+		is_collectible(mlx, y, x - 1);
+		is_exit(mlx, y, x - 1);
 	}
-	if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS && g_map.tab[y][x + 1] != '1')
+	else if (keydata.key == MLX_KEY_D && keydata.action == MLX_PRESS && g_map.tab[y][x + 1] != '1')
 	{
-		g_map.tab[y][x] = '0';
-		key_event(mlx, y, x + 1);
+		g_images.image_character->instances->x += g_pixels;
+		is_collectible(mlx, y, x + 1);
+		is_exit(mlx, y, x + 1);
 	}
 		
 }
@@ -135,8 +156,10 @@ int	so_long(char *file)
 
 	g_map = map_parser(file);
 	mlx = mlx_init(g_map.x * g_pixels, g_map.y * g_pixels, "game", true);
-	
+	g_player = init_player();
+
 	display_map(mlx);
+	display_character(mlx, g_player.y, g_player.x);
 
 	mlx_key_hook(mlx, &key_hook, mlx);
 	mlx_loop(mlx);
